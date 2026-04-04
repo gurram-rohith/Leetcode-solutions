cc20c626985813764e65e4473e98d40bde7f8e38
Rohith Gurram <rohithsony123@gmail.com>
2026-04-05 00:14:33 +0530

    Create 219-ContainsDuplicateIi

---
+++ 219-ContainsDuplicateIi/219-ContainsDuplicateIi.cpp
@@ -0,0 +1,20 @@
+// Last updated: 4/5/2026, 12:14:33 AM
+class Solution {
+public:
+    bool containsNearbyDuplicate(vector<int>& nums, int k) {
+        unordered_set<int> st;
+
+        for(int i = 0; i < nums.size(); i++) {
+
+            if(st.count(nums[i]))
+                return true;
+
+            st.insert(nums[i]);
+
+            if(st.size() > k)
+                st.erase(nums[i-k]);
+        }
+
+        return false;
+    }
+};
