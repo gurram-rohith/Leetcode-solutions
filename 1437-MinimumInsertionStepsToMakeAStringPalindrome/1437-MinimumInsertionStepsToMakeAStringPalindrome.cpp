4d1e6ef818cb6259d083db2648716ad4e873c095
Rohith Gurram <rohithsony123@gmail.com>
2026-04-05 00:13:18 +0530

    Create 1437-MinimumInsertionStepsToMakeAStringPalindrome

---
+++ 1437-MinimumInsertionStepsToMakeAStringPalindrome/1437-MinimumInsertionStepsToMakeAStringPalindrome.cpp
@@ -0,0 +1,36 @@
+// Last updated: 4/5/2026, 12:13:18 AM
+class Solution {
+public:
+    int lcs(string str1, string str2) {
+      int n=str1.size();
+      int m=str2.size();
+
+      vector<vector<int>>dp(n+1,vector<int>(m+1,0));
+      string ans;
+      for(int i=0;i<=n;i++) dp[i][0]=0;
+      for(int j=0;j<=m;j++) dp[0][j]=0;
+
+      for(int i=1;i<=n;i++)
+      {
+        for(int j=1;j<=m;j++)
+        {
+          int match=0,notmatch=0;
+          if(str1[i-1]==str2[j-1])
+          match=1+dp[i-1][j-1];
+          else
+          notmatch=max(dp[i-1][j],dp[i][j-1]);
+          dp[i][j]=max(match,notmatch);
+        }
+      }
+      
+      return dp[n][m];
+    }
+    int longestPalindromeSubseq(string s) {
+        string s2=s;
+        reverse(s2.begin(),s2.end());
+        return lcs(s,s2);
+    }
+    int minInsertions(string s) {
+        return s.size()-longestPalindromeSubseq(s);
+    }
+};
