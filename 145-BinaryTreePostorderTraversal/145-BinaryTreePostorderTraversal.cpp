d67b0c0c3f13b6b4e5de09085a770a9e8714bbeb
Rohith Gurram <rohithsony123@gmail.com>
2026-04-05 00:14:51 +0530

    Create 145-BinaryTreePostorderTraversal

---
+++ 145-BinaryTreePostorderTraversal/145-BinaryTreePostorderTraversal.cpp
@@ -0,0 +1,28 @@
+// Last updated: 4/5/2026, 12:14:51 AM
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+void postOrder(TreeNode*root,vector<int>&ans){
+    if(root==NULL) return ;
+    else
+    {postOrder(root->left,ans);
+    postOrder(root->right,ans);
+    ans.push_back(root->val);}
+    
+}
+    vector<int> postorderTraversal(TreeNode* root) {
+        vector<int>ans;
+        postOrder(root,ans);
+        return ans;
+    }
+};
