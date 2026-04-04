671aa239bfdcf0fd87f33a5e9b0e8f3edb41f532
Rohith Gurram <rohithsony123@gmail.com>
2026-04-05 00:15:41 +0530

    Create 44-WildcardMatching

---
+++ 44-WildcardMatching/44-WildcardMatching.cpp
@@ -0,0 +1,62 @@
+// Last updated: 4/5/2026, 12:15:41 AM
+class Solution {
+public:
+    // bool isTrue(string &s,string &p,int i,int j,vector<vector<int>>&dp)
+    // {
+    //     if(i<0&&j<0) return true;
+    //     if(j<0) return false;
+    //     if(i<0)
+    //     {
+    //         for(int k=j;k>=0;k--)
+    //         {
+    //             if(p[k]!='*') return false;
+    //         }
+    //         return true;
+    //     }
+    //     if(dp[i][j]!=-1) return dp[i][j];
+    //     if(s[i]==p[j]||(p[j]=='?'))
+    //     {
+    //         return dp[i][j]=isTrue(s,p,i-1,j-1,dp);
+    //     }
+    //     else{
+    //         if(p[j]!='*')
+    //         return dp[i][j]=false;
+    //         if(p[j]=='*')
+    //         return dp[i][j]=isTrue(s,p,i-1,j,dp)||(isTrue(s,p,i,j-1,dp));
+    //     }
+    //     return dp[i][j];
+    // }
+    bool isMatch(string s, string p) {
+        int n=s.size(),m=p.size();
+        vector<vector<bool>>dp(n+1,vector<bool>(m+1,false));
+        dp[0][0]=true;
+        
+        int j;
+        for( j=1;j<=m;j++)
+        {
+            if(p[j-1]=='*')
+            {
+                dp[0][j]=dp[0][j-1];
+            }
+        }
+        for(int i=1;i<=n;i++)
+        dp[i][0]=false;
+        for(int i=1;i<=n;i++)
+        {
+            for(int j=1;j<=m;j++)
+            {
+                if(s[i-1]==p[j-1]||(p[j-1]=='?'))
+                dp[i][j]=dp[i-1][j-1];
+                else
+                {
+                    if(p[j-1]!='*')
+                    dp[i][j]=false;
+                    else
+                    dp[i][j]=(dp[i-1][j])||(dp[i][j-1]);
+                }
+            }
+        }
+        return dp[n][m];
+
+    }
+};
