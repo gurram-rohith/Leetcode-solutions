fabe3bab64b9d5e027351865b86eec4f75e1e160
Rohith Gurram <rohithsony123@gmail.com>
2026-04-19 19:46:57 +0530

    Create 1855-MaximumDistanceBetweenaPairofValues

---
+++ 1855-MaximumDistanceBetweenaPairofValues/version/cpp/1855-MaximumDistanceBetweenaPairofValues_20260419_194656.cpp
@@ -0,0 +1,27 @@
+// Last updated: 4/19/2026, 7:46:56 PM
+1class Solution {
+2public:
+3    int binsearch(int num,vector<int>&nums2,int i)
+4    {
+5        int low=i,high=nums2.size()-1,ans=0;
+6        while(low<=high)
+7        {
+8            int mid=low+(high-low)/2;
+9            if(num<=nums2[mid])
+10            {
+11                ans=mid;
+12                low=mid+1;
+13            }
+14            else high=mid-1;
+15        }
+16        return ans;
+17    }
+18    int maxDistance(vector<int>& nums1, vector<int>& nums2) {
+19        int maxdist=0;
+20        for(int i=0;i<nums1.size();i++)
+21        {
+22            maxdist=max(maxdist,binsearch(nums1[i],nums2,i)-i);
+23        }
+24        return maxdist;
+25    }
+26};
