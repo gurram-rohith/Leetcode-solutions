6073d4f3cdf3287a0dffe1a0dd527b54dd655231
Rohith Gurram <rohithsony123@gmail.com>
2026-06-15 18:34:51 +0530

    Time: 15 ms (37.11%), Space: 20.9 MB (9.59%) - LeetHub

--- 1340-jump-game-v/1340-jump-game-v.cpp
+++ 1340-jump-game-v/1340-jump-game-v.cpp
@@ -1,31 +1,27 @@
 class Solution {
 public:
-    int jumps(vector<int>&dp,int i,int d,vector<int>&arr)
-    {
-        if(dp[i]!=-1) return dp[i];
-        dp[i]=1;
-        int n=arr.size();
-        for(int j=i+1;j<=min(i+d,n-1);j++)
-        {
-            if(arr[j]>=arr[i]) break;
-            dp[i]=max(dp[i],1+jumps(dp,j,d,arr));
-        }
-        for(int j=i-1;j>=max(i-d,0);j--)
-        {
-            if(arr[j]>=arr[i]) break;
-            dp[i]=max(dp[i],1+jumps(dp,j,d,arr));
-        }
-        return dp[i];
-
-    }
     int maxJumps(vector<int>& arr, int d) {
         int n=arr.size();
-        vector<int>dp(n,-1);
+        vector<int>dp(n,1);
         
         int ans=1;
-        for(int i=0;i<n;i++) 
+        vector<pair<int,int>>vpp;
+        for(int i=0;i<n;i++) vpp.push_back({arr[i],i});
+        sort(vpp.begin(),vpp.end());
+        for(auto it:vpp)
         {
-            ans=max(ans,jumps(dp,i,d,arr));
+            int i=it.second;
+           for(int j=i+1;j<=min(i+d,n-1);j++)
+            {
+            if(arr[j]>=arr[i]) break;
+            dp[i]=max(dp[i],1+dp[j]);
+            }
+            for(int j=i-1;j>=max(i-d,0);j--)
+            {
+            if(arr[j]>=arr[i]) break;
+            dp[i]=max(dp[i],1+dp[j]);
+            }
+            ans=max(ans,dp[i]);
         }
         return ans;
 
