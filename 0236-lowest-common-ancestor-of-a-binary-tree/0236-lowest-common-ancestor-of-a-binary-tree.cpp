0f95933a3b3379226570823aa6bd1840cba4f9fb
Rohith Gurram <rohithsony123@gmail.com>
2026-07-11 22:27:11 +0530

    Time: 51 ms (66.45%), Space: 44 MB (36.87%) - LeetHub

--- 0236-lowest-common-ancestor-of-a-binary-tree/0236-lowest-common-ancestor-of-a-binary-tree.cpp
+++ 0236-lowest-common-ancestor-of-a-binary-tree/0236-lowest-common-ancestor-of-a-binary-tree.cpp
@@ -9,41 +9,12 @@
  */
 class Solution {
 public:
-    void traverse(TreeNode*root,map<TreeNode*,TreeNode*>&parent)
-    {
-        if(root==NULL) return;
-        if(root->left!=NULL)
-        parent[root->left]=root;
-        if(root->right!=NULL) parent[root->right]=root;
-        traverse(root->left,parent);
-        traverse(root->right,parent);
-    }
     TreeNode* lowestCommonAncestor(TreeNode* root, TreeNode* p, TreeNode* q) {
-        map<TreeNode*,TreeNode*>parent;
-        parent[root]=NULL;
-        traverse(root,parent);
-        vector<TreeNode*>f,s;
-        TreeNode*tempp=p,*tempq=q;
-        while(parent[p]!=NULL)
-        f.push_back(p),p=parent[p];
-        while(parent[q]!=NULL)
-        s.push_back(q),q=parent[q];
-        reverse(f.begin(),f.end());
-        reverse(s.begin(),s.end());
-        unordered_set<TreeNode*>st;
-        while(tempp!=NULL)
-        {
-            st.insert(tempp);
-            tempp=parent[tempp];
-        }
-        while(tempq!=NULL)
-        {
-            if(st.count(tempq)) return tempq;
-            tempq=parent[tempq];
-        }
-        return p;
-
-        
-
+        if(root==NULL||root==p||root==q) return root;
+        TreeNode*l= lowestCommonAncestor(root->left,p,q);
+        TreeNode*r=lowestCommonAncestor(root->right,p,q);
+        if(l==NULL) return r;
+        else if(r==NULL) return l;
+        return root;
     }
 };
