8d8f7c19a633229253edabedc347dc811623830c
Rohith Gurram <rohithsony123@gmail.com>
2026-07-23 22:23:05 +0530

    Create 701-InsertintoaBinarySearchTree

---
+++ 701-InsertintoaBinarySearchTree/version/cpp/701-InsertintoaBinarySearchTree_20260723_222305.cpp
@@ -0,0 +1,28 @@
+// Last updated: 7/23/2026, 10:23:05 PM
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    TreeNode* insertIntoBST(TreeNode* root, int val) {
+        if(root==NULL){
+            return new TreeNode(val);
+        }
+        if(val<root->val){
+            root->left = insertIntoBST(root->left,val);
+        }
+        else{
+            root->right = insertIntoBST(root->right,val);
+        }
+        return root;
+        
+    }
+};
