b51fbb4bcda68e83c1505b41202b6c5cb10b4d85
Rohith Gurram <rohithsony123@gmail.com>
2026-08-11 20:05:50 +0530

    Time: 1 ms (80.42%), Space: 18.7 MB (39.82%) - LeetHub

--- 0210-course-schedule-ii/0210-course-schedule-ii.cpp
+++ 0210-course-schedule-ii/0210-course-schedule-ii.cpp
@@ -1,42 +1,56 @@
 class Solution {
 public:
-    vector<int>order;
-    bool detectCycle(vector<vector<int>>&adj,int node,vector<bool>&vis,vector<bool>&pathvis)
+    bool detect(vector<vector<int>>&adj,int node,vector<bool>&vis1,vector<bool>&pathvis)
     {
-        vis[node]=true;
+        vis1[node]=true;
         pathvis[node]=true;
-        
         for(int i:adj[node])
         {
-            if(vis[i])
-            {
-                if(pathvis[i]) return true;
-            }
-            else {
-                if(detectCycle(adj,i,vis,pathvis)==true) return true;
-            }
+           if(vis1[i])
+           {
+            if(pathvis[i]) return true;
+           }
+           else
+           {
+             if(detect(adj,i,vis1,pathvis)==true) return true;
+           }
         }
-        order.push_back(node);
         pathvis[node]=false;
         return false;
-
     }
-
+    void dfs(int node,vector<vector<int>>&adj,vector<bool>&vis,stack<int>&st)
+    {
+        vis[node]=true;
+        for(int i:adj[node])
+        {
+            if(!vis[i]) dfs(i,adj,vis,st);
+        }
+        st.push(node);
+    }
     vector<int> findOrder(int numCourses, vector<vector<int>>& prerequisites) {
         int n=numCourses;
+        vector<bool>vis1(n,false),vis(n,false),pathvis(n,false);
         vector<vector<int>>adj(n);
+        vector<int>ans;
+
         for(int i=0;i<prerequisites.size();i++)
+        adj[prerequisites[i][1]].push_back(prerequisites[i][0]);
+
+        stack<int>st;
+        for(int i=0;i<n;i++)
         {
-            adj[prerequisites[i][1]].push_back(prerequisites[i][0]);
-        }
-        vector<bool>vis(n,false),pathvis(n,false);
-        for(int i=0;i<numCourses;i++)
-        {
-            if(!vis[i]) {
-                if(detectCycle(adj,i,vis,pathvis)==true) return {};
+            if(!vis[i])
+            dfs(i,adj,vis,st);
+            if(!vis1[i]){
+                if(detect(adj,i,vis1,pathvis)==true) return {};
             }
         }
-        reverse(order.begin(),order.end());
-        return order;
+        while(!st.empty())
+        {
+            ans.push_back(st.top());
+            st.pop();
+        }
+        return ans;
+
     }
 };
